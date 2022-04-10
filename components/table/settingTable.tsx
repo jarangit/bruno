60de04298975378263c619f1716a974755cadab5
jarangit <jaran.dch@gmail.com>
2022-04-10 11:03:03 +0700

    add but

--- components/table/settingTable.tsx
+++ components/table/settingTable.tsx
@@ -5,59 +5,67 @@ type Props = {}
 const SettingTable = (props: Props) => {
   const data = [1, 2, 3, 4, 5]
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
+    <>
 
-          <th>Priority</th>
-
-          <th>Name</th>
-
-          <th>Time Zone</th>
-
-          <th>Unit Price</th>
-
-        </tr>
-      </thead>
-
-
-      <tbody>
-        {data.map((item, key) => (
+      <table className={styles.table}>
+        <thead>
           <tr>
 
-            <td>1</td>
+            <th>Priority</th>
 
-            <td>
-              <div className={styles.name}>
-                <p>
-                  Nomal
-                </p>
-              </div>
-            </td>
+            <th>Name</th>
 
-            <td>
-              <div className={styles.fillSelect}>
-                <select name="startTime" id="startTime" className='selectGray'>
-                  <option value="">11:00</option>
-                </select>
-                <p>To</p>
-                <select name="startTime" id="startTime" className='selectGray'>
-                  <option value="">11:00</option>
-                </select>
-              </div>
-            </td>
+            <th>Time Zone</th>
 
-            <td>
-              <div className={styles.unitPrice}>
-                <p>1</p>
-                <span>THB/kWh</span>
-              </div>
-            </td>
+            <th>Unit Price</th>
+
           </tr>
-        ))}
+        </thead>
 
-      </tbody>
-    </table>
+
+        <tbody>
+          {data.map((item, key) => (
+            <tr>
+
+              <td>1</td>
+
+              <td>
+                <div className={styles.name}>
+                  <p>
+                    Nomal
+                  </p>
+                </div>
+              </td>
+
+              <td>
+                <div className={styles.fillSelect}>
+                  <select name="startTime" id="startTime" className='selectGray'>
+                    <option value="">11:00</option>
+                  </select>
+                  <p>To</p>
+                  <select name="startTime" id="startTime" className='selectGray'>
+                    <option value="">11:00</option>
+                  </select>
+                </div>
+              </td>
+
+              <td>
+                <div className={styles.unitPrice}>
+                  <p>1</p>
+                  <span>THB/kWh</span>
+                </div>
+              </td>
+            </tr>
+          ))}
+
+        </tbody>
+
+      </table>
+
+      <div style={{ textAlign: "center", marginTop: "20px" }} >
+        <button className='but_gray' >บันทึก</button>
+      </div>
+    </>
   )
 }
 
