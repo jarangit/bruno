328d9affee3858dd6d924486a3cf728f82113be9
jarangit <jaran.dch@gmail.com>
2022-05-01 14:31:40 +0700

    call api and mock id for query data

--- utills/fecthApi.ts
+++ utills/fecthApi.ts
@@ -5,7 +5,7 @@ export const baseUrl = "https://api.airin1.com/api/";
 export const fecthApi = async (url: string) => {
   const { data } = await axios.get(url, {
     headers: {
-      "Authorization": `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDg0NTU2MjcsImV4cCI6MTY1MTA0NzYyNywib3RwIjoiIiwidWlkIjo0M30.8UVfTKIS0l5c0lpjeIBItMeVW-bxV5tr17_wzhCW0tA `,
+      "Authorization": `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTEzOTAxNTksImV4cCI6MTY1Mzk4MjE1OSwib3RwIjoiIiwidWlkIjo0M30.7CPKEEGsbgz37RkSlC3b2zq1jLkp9gRChaVqvDAqiPk`,
     },
   });
 
